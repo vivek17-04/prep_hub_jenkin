10a396fb01d7e101314402e71f83f4d02392b4d5
vivek <vkapashera77@gmail.com>
2024-02-16 22:52:12 +0530

    third

--- prep.py
+++ prep.py
@@ -1,2 +1,3 @@
 print("Hello, Today we are going to learn about jenkins and their deployment into github and their respective jenkins")
 print("I am adding new line to track the changes")
+print("I am adding new line to commit into master")
